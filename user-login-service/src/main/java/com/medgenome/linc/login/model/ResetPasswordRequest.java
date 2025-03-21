b8f3e921f8fcadfab253b9b974d057f262910243
mukeshchandravanshi <mukesh.chandravanshi@clairlabs.ai>
2025-03-21 12:58:57 +0530

    removed unused entities

--- user-login-service/src/main/java/com/medgenome/linc/login/model/ResetPasswordRequest.java
+++ user-login-service/src/main/java/com/medgenome/linc/login/model/ResetPasswordRequest.java
@@ -5,15 +5,9 @@ import lombok.Data;
 
 @Data
 public class ResetPasswordRequest {
-
-    @NotBlank(message = "Email or phone number is required.")
-    private String emailOrPhone;
-
+    
     @NotBlank(message = "OTP is required.")
     private String otp;
-
-    @NotBlank(message = "Old password is required.")
-    private String oldPassword;
 
     @NotBlank(message = "New password is required.")
     private String newPassword;
