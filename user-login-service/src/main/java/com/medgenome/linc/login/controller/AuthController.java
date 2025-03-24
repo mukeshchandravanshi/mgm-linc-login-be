c8390bd960fb1f4814d9b90056160c3dadffb5a6
mukeshchandravanshi <mukesh.chandravanshi@clairlabs.ai>
2025-03-24 21:27:48 +0530

    created login-another-way endpoint

--- user-login-service/src/main/java/com/medgenome/linc/login/controller/AuthController.java
+++ user-login-service/src/main/java/com/medgenome/linc/login/controller/AuthController.java
@@ -91,81 +91,6 @@ public class AuthController {
         return Map.of("token", token, "message", "You are successfully registered to MedGenome.");
     }
 
-
-    @PostMapping("/loginOld")
-    public Map<String, String> loginOld(@RequestBody User request) {
-        String email = request.getEmail();
-        String phoneNum = request.getPhoneNum();
-        String password = request.getPassword();
-
-        // Define regex patterns for email and phone validation
-        String emailRegex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
-        String phoneRegex = "^\\+?[0-9\\-\\s]{7,15}$";
-
-        if (email != null && !Pattern.matches(emailRegex, email)) {
-            throw new RuntimeException("Invalid email format.");
-        }
-
-        if (phoneNum != null && !Pattern.matches(phoneRegex, phoneNum)) {
-            throw new RuntimeException("Invalid phone number format.");
-        }
-
-        String userName = email != null ? email : phoneNum;
-
-        Optional<User> userOpt = userService.findByUserName(userName);
-
-        if (userOpt.isEmpty()) {
-            throw new RuntimeException(email != null ? "Invalid email." : "Invalid phone number.");
-        }
-
-        User user = userOpt.get();
-
-        if (!passwordEncoder.matches(password, user.getPassword())) {
-            throw new RuntimeException("Invalid password.");
-        }
-
-        String token = jwtUtil.generateToken(userName);
-        return Map.of("token", token, "message", "Welcome to MedGenome!");
-    }
-
-    @PostMapping("/forgot-password")
-    public ResponseEntity<?> forgotPassword(@RequestBody ForgotPasswordRequest request) {
-        try {
-            String emailOrPhone = request.getEmailOrPhone();
-            Optional<User> userOpt = userService.findByUserName(emailOrPhone);
-
-            if (userOpt.isEmpty()) {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                        .body(Map.of("message", "User not found with provided email or phone number."));
-            }
-
-            User user = userOpt.get();
-
-            // OTP based
-            String otp = otpUtil.generateOtp(emailOrPhone);
-            String message = "You have requested to reset your password. Use the OTP below to proceed with the reset: ";
-            String subjectMessage = "Password Reset Request";
-
-            if (emailOrPhone.contains("@")) {
-                emailService.sendResetPasswordEmail(user.getEmail(), subjectMessage, message + otp);
-            } else {
-                try {
-                    smsService.sendSms(user.getPhoneNum(), "Reset your password using this OTP: " + otp);
-                } catch (Exception smsException) {
-                    System.err.println("AuthController: Failed to send SMS : " + smsException.getMessage());
-                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                            .body(Map.of("message", "Failed to send SMS. Please try again later."));
-                }
-            }
-
-            return ResponseEntity.ok(Map.of("message", "Password reset OTP sent successfully!"));
-        } catch (Exception emailException) {
-            System.err.println("AuthController: Failed to send EMAIL: " + emailException.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(Map.of("message", "Failed to send Email. Please try again later.."));
-        }
-    }
-
     @PostMapping("/login")
     public ResponseEntity<Map<String, String>> login(@RequestBody User request) {
         String email = request.getEmail();
@@ -224,6 +149,51 @@ public class AuthController {
         return ResponseEntity.ok(Map.of("message", "OTP sent successfully via Email and SMS!"));
     }
 
+    @PostMapping("/login-another-way")
+    public ResponseEntity<Map<String, String>> sendOtp(@RequestBody User request) {
+        String email = request.getEmail();
+        String phoneNum = request.getPhoneNum();
+        String emailOrPhone = email != null ? email : phoneNum;
+
+        if (email != null && email.isBlank()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(Map.of("message", "Email cannot be blank."));
+        }
+
+        if (phoneNum != null && phoneNum.isBlank()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(Map.of("message", "Phone Number cannot be blank."));
+        }
+
+        // Validate email or phone number format
+        emailAndPhoneValidator.validateEmailAndPhone(email, phoneNum);
+
+        // Check if user exists
+        Optional<User> userOpt = userService.findByUserName(emailOrPhone);
+
+        if (userOpt.isEmpty()) {
+            throw new RuntimeException(email != null ? "Invalid email." : "Invalid phone number.");
+        }
+
+        User user = userOpt.get();
+        String otp = otpUtil.generateOtp(emailOrPhone);
+        String subjectMessage = "Login OTP";
+
+        // Send OTP via Email or SMS
+        try {
+            if (emailOrPhone.contains("@")) {
+                emailService.sendResetPasswordEmail(user.getEmail(), subjectMessage, "Your OTP: " + otp);
+            } else {
+                smsService.sendSms(user.getPhoneNum(), "Your OTP: " + otp);
+            }
+            return ResponseEntity.ok(Map.of("message", "OTP sent successfully!"));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Map.of("message", "Failed to send OTP. Please try again later."));
+        }
+    }
+
+
     @PostMapping("/verify-otp")
     public ResponseEntity<Map<String, String>> verifyOtp(@RequestBody OtpVerificationRequest request) {
         String otp = request.getOtp();
@@ -251,41 +221,46 @@ public class AuthController {
         return ResponseEntity.ok(Map.of("token", loginToken, "message", "Login successful!"));
     }
 
+    @PostMapping("/forgot-password")
+    public ResponseEntity<?> forgotPassword(@RequestBody ForgotPasswordRequest request) {
+        try {
+            String emailOrPhone = request.getEmailOrPhone();
+            Optional<User> userOpt = userService.findByUserName(emailOrPhone);
+
+            if (userOpt.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body(Map.of("message", "User not found with provided email or phone number."));
+            }
+
+            User user = userOpt.get();
+
+            // OTP based
+            String otp = otpUtil.generateOtp(emailOrPhone);
+            String message = "You have requested to reset your password. Use the OTP below to proceed with the reset: ";
+            String subjectMessage = "Password Reset Request";
+
+            if (emailOrPhone.contains("@")) {
+                emailService.sendResetPasswordEmail(user.getEmail(), subjectMessage, message + otp);
+            } else {
+                try {
+                    smsService.sendSms(user.getPhoneNum(), "Reset your password using this OTP: " + otp);
+                } catch (Exception smsException) {
+                    System.err.println("AuthController: Failed to send SMS : " + smsException.getMessage());
+                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                            .body(Map.of("message", "Failed to send SMS. Please try again later."));
+                }
+            }
+
+            return ResponseEntity.ok(Map.of("message", "Password reset OTP sent successfully!"));
+        } catch (Exception emailException) {
+            System.err.println("AuthController: Failed to send EMAIL: " + emailException.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Map.of("message", "Failed to send Email. Please try again later.."));
+        }
+    }
 
 
 
-
-
-//    @PostMapping("/login-another-way")
-//    public Map<String, String> loginAnotherWay(@RequestBody User request) {
-//        String emailOrPhone = request.getEmail()!=null?request.getEmail():request.getPhoneNum();
-//        if (emailOrPhone == null || emailOrPhone.isBlank()) {
-//            throw new RuntimeException("Email or phone number is required.");
-//        }
-//
-//        // Validate email or phone number format
-//        if (emailOrPhone.contains("@")) {
-//            if (!Pattern.matches("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$", emailOrPhone)) {
-//                throw new RuntimeException("Invalid email format.");
-//            }
-//        } else {
-//            if (!Pattern.matches("^\\+?[0-9\\-\\s]{7,15}$", emailOrPhone)) {
-//                throw new RuntimeException("Invalid phone number format.");
-//            }
-//        }
-//
-//        Optional<User> userOpt = userService.findByUserName(emailOrPhone);
-//
-//        if (userOpt.isEmpty()) {
-//            throw new RuntimeException("No user found with the provided email or phone number.");
-//        }
-//
-//        // Generate and send OTP
-//        String otp = otpService.generateOtp(emailOrPhone);
-//        otpService.sendOtp(emailOrPhone, otp);
-//
-//        return Map.of("message", "OTP has been sent to your registered contact.");
-//    }
 
 
 //    @PostMapping("/reset-password")
